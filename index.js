897a7bcd83695688f84abb487e1b2575deaa863c
Briana Baker <brianaclairbaker@gmail.com>
2017-11-13 20:52:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {key: "value"}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign({}, object, {[key]: "value"});
+  Object.assign({}, object, {key: "value"});
   return object;
 }
